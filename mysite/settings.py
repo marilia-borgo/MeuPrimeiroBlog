f0368929a5045091f24ad6cd7d3ac2a7f7e54994
marilia-borgo <marilia.bm01@gmail.com>
2022-06-29 14:42:29 -0300

    secret key escondida, variavel de ambiente

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+kv%h-#v+^50=-xd9bql69uzbc+%t+qi$^8wd8)e^i^or-_ckx'
+SECRET_KEY = os.getenv('SECRET_KEY','secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
